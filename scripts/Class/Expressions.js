4d1fe6f522c8980ae05a5740219ad00980acc02e
Christian de Oliveira <christianoliveira1908@hotmail.com>
2020-12-22 21:10:09 -0300

    Algoritmo para montar as equações para o jogador realizar.

--- scripts/Class/Expressions.js
+++ scripts/Class/Expressions.js
@@ -1,5 +1,5 @@
 class Expressions {
-    listExpressions = ['+', '*', '/', '^'];
+    listExpressions = ['+', '*', '/', '-'];
 
     getRandomExpression = () => {
         const randomIndex = Math.floor(Math.random() * this.listExpressions.length);
@@ -8,25 +8,82 @@ class Expressions {
     }
 
     createExpression = (difficulty) => {
-        let qtNumbersInExpression;
+        let qtNumbersInExpression, maxNumber, qtItens;
+
+        const possibilites = [];
 
         switch (difficulty) {
             case Difficulties.EASY: {
                 qtNumbersInExpression = 2;
+                qtItens = 4;
+                maxNumber = 100;
                 break;
             }
             case Difficulties.NORMAL: {
                 qtNumbersInExpression = 3;
+                qtItens = 8;
+                maxNumber = 500;
                 break;
             }
             case Difficulties.HARD: {
                 qtNumbersInExpression = 4;
+                qtItens = 12;
+                maxNumber = 1000;
                 break;
             }
         }
 
-        for (let i = 0; i < qtNumbersInExpression; i++) {
+        let createdExpression = "";
 
+        for (let i = 1; i <= qtNumbersInExpression; i++) {
+            const number = generateRandomNumber(1, maxNumber);
+
+            const expression = this.getRandomExpression();
+
+            if (i < qtNumbersInExpression) {
+                createdExpression += `${number}${expression}`;
+            }
+            else {
+                createdExpression += `${number}`;
+            }
         }
+
+        const expressionResult = parseFloat(math.evaluate(createdExpression)).toFixed(2);
+
+        possibilites.push({
+            value: expressionResult,
+            isCorrectAnswer: true
+        });
+
+        let qtToGenerate = qtItens - 1;
+
+        for (let i = 0; i < qtToGenerate; i++) {
+            let number;
+
+            if (expressionResult < 0) {
+                number = generateRandomNumber(-maxNumber, 0);
+            }
+            else {
+                number = generateRandomNumber(0, maxNumber);
+            }
+
+            number = parseFloat(number).toFixed(2);
+
+            possibilites.push({
+                value: number,
+                isCorrectAnswer: false
+            });
+        }
+
+        possibilites.reverse();
+        possibilites.sort();
+
+        return {
+            expression: createdExpression,
+            possibilites,
+            qtItens
+        };
     }
+
+
 }
